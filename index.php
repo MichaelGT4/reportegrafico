fbd2b11daa77a9f1bad332bfe29e50e0bb8b93a4
Michael <michael13117764@gmail.com>
2020-06-03 19:49:20 -0400

    Dk

--- index.php
+++ index.php
@@ -4,6 +4,20 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>ssss</title>
+
+    <!--Estilos del grafico -->
+    <link rel="stylesheet" href="/css/anychart-font.css">
+    <link rel="stylesheet" href="/css/anychart-ui.min.css">
+
+    <!--Script de los graficos-->
+    <script src="/js/anychart-base.min.js"></script>
+    <script src="/js/anychart-exports.min.js"></script>
+    <script src="/js/anychart-ui.min.js"></script>
+
+    <!--CSS-->
+    <style> html, body, #container 
+    width 
+    </style>
 </head>
 <body>
     
