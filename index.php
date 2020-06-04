f54f6b295b9a7b4ef4d4f757914fd1cf8c9e7553
Michael <michael13117764@gmail.com>
2020-06-03 20:23:28 -0400

    grafico de barras  y pastel finalizado

--- index.php
+++ index.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>ssss</title>
+    <title>Graficos</title>
 
     <!--Estilos del grafico -->
     <link rel="stylesheet" href="./css/anychart-font.css">
